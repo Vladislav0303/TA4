e51a0b3dd11411d45f5aef95b5046771bc72d2ab
Vladislav0303 <vladshulyak003@gmail.com>
2025-06-09 16:59:53 +0300

    Add files via upload

---
+++ WeightBalance.java
@@ -0,0 +1,23 @@
+/*
+Умова:
+Є гирі {1, 3, 3, 5, 9}.
+Потрібно набрати масу W, використовуючи найменшу кількість гир.
+ */
+public class WeightBalance {
+    public static void main(String[] args) {
+        int[] weights = {9, 5, 3, 3, 1};
+        int W = 22;
+        int count = 0;
+
+        for (int i = 0; i < weights.length; i++) {
+            while (W >= weights[i]) {
+                W -= weights[i];
+                count++;
+                weights[i] = 0;
+                break;
+            }
+        }
+
+        System.out.println("Гир використано: " + count);
+    }
+}
